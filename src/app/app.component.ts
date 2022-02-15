da6e35981eebb9566d1a7b644c7d367970978e7f
Jakub <kubhinio@gmail.com>
2022-02-15 15:00:42 +0100

    added static view of application and animated add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'best-quotes';
+  showForm = false;
+
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
 }
