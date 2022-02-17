d773e61bb0d1ee59314d796e30e2285e2174baa2
Jakub <kubhinio@gmail.com>
2022-02-17 19:14:50 +0100

    added vote-buttons component
    modified app component - replace two buttons component to vote-buttons component

---
+++ src/app/votes-button/votes-button.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-votes-button',
+  templateUrl: './votes-button.component.html',
+  styleUrls: ['./votes-button.component.css']
+})
+export class VotesButtonComponent {
+
+  @Output() voteValue = new EventEmitter<number>();
+}
